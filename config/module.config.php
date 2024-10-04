29ebc6b940e6abd95462dfa6fe68229d1223f357
John Flatness <john@zerocrates.org>
2024-10-04 16:08:05 -0400

    Add ExtractMetadata support

--- config/module.config.php
+++ config/module.config.php
@@ -1,12 +1,19 @@
 <?php
+namespace OhmsEmbed;
+
 return [
     'file_renderers' => [
         'invokables' => [
-            'ohms_embed' => 'OhmsEmbed\Media\FileRenderer\OhmsEmbedRenderer',
+            'ohms_embed' => Media\FileRenderer\OhmsEmbedRenderer::class,
         ],
         'aliases' => [
             'application/xml' => 'ohms_embed',
             'xml' => 'ohms_embed',
+        ],
+    ],
+    'extract_metadata_extractors' => [
+        'invokables' => [
+            'ohms' => Extractor\Ohms::class,
         ],
     ],
 ];
---
+++ src/Extractor/Ohms.php
@@ -0,0 +1,103 @@
+<?php
+namespace OhmsEmbed\Extractor;
+
+use DomDocument;
+use DomXpath;
+use ExtractMetadata\Extractor\ExtractorInterface;
+
+class Ohms implements ExtractorInterface
+{
+    public function getLabel()
+    {
+        return 'OHMS';
+    }
+
+    public function isAvailable()
+    {
+        return extension_loaded('dom');
+    }
+
+    public function supports($mediaType)
+    {
+        return in_array($mediaType, [
+            'text/xml',
+            'application/xml',
+        ]);
+    }
+
+    public function extract($filePath, $mediaType)
+    {
+        $metadata = [];
+        $doc = new DomDocument;
+        $doc->load($filePath);
+
+        $record = $doc->documentElement->childNodes->item(0);
+        if (!$record || !$record->tagName === 'record') {
+            return $metadata;
+        }
+
+        $xpath = new DOMXPath($doc);
+        $xpath->registerNamespace('o', 'https://www.weareavp.com/nunncenter/ohms');
+
+        $xpaths = [
+            'id' => 'string(@id)',
+            'dt' => 'string(@dt)',
+            'version' => 'string(o:version)',
+            'date' => 'string(o:date/@value)',
+            'cms_record_id' => 'string(ocms_record_id)',
+            'title' => 'string(o:title)',
+            'accession' => 'string(o:accession)',
+            'duration' => 'string(o:duration)',
+            'collection_id' => 'string(o:collection_id)',
+            'collection_name' => 'string(o:collection_name)',
+            'series_id' => 'string(o:series_id)',
+            'series_name' => 'string(o:series_name)',
+            'repository' => 'string(o:repository)',
+            'funding' => 'string(o:funding)',
+            'repository_url' => 'string(o:repository_url)',
+            'file_name' => 'string(o:file_name)',
+            'transcript_alt_lang' => 'string(o:transcript_alt_lang)',
+            'language' => 'string(o:language)',
+            'user_notes' => 'string(o:user_notes)',
+            'type' => 'string(o:type)',
+            'description' => 'string(o:description)',
+            'rel' => 'string(o:rel)',
+            'rights' => 'string(o:rights)',
+            'fmt' => 'string(o:fmt)',
+            'usage' => 'string(o:usage)',
+            'xmllocation' => 'string(o:xmllocation)',
+            'xmlfilename' => 'string(o:xmlfilename)',
+            'collection_link' => 'string(o:collection_link)',
+            'series_link' => 'string(o:series_link)',
+        ];
+
+        foreach ($xpaths as $key => $xpathQuery) {
+            $result = $xpath->evaluate($xpathQuery, $record);
+            if (!is_string($result) || $result === '') {
+                continue;
+            }
+            $metadata[$key] = $result;
+        }
+
+        $xpathsMulti = [
+            'subject' => 'o:subject',
+            'keyword' => 'o:keyword',
+            'interviewee' => 'o:interviewee',
+            'interviewer' => 'o:interviewer',
+            'format' => 'o:format',
+        ];
+
+        foreach ($xpathsMulti as $key => $xpathQuery) {
+            $result = $xpath->query($xpathQuery, $record);
+            foreach ($result as $element) {
+                $text = $element->textContent;
+                if ($text === '') {
+                    continue;
+                }
+                $metadata[$key][] = $text;
+            }
+        }
+
+        return $metadata;
+    }
+}
